a35492038acbab5272bc7ec0daf2e93d5c7cc0fb
Tomasz Mikolajewski <asanoki@gmail.com>
2012-03-21 17:55:41 +0100

    - Moved to Autotools. - Fixed memory leaks in encoding.cpp (boost::shared_ptr). - N-gram dictionaries for Japanese language provided as an example. - Disabled TCP buffering (TCP_NODELAY) for lower latency.

---
+++ tools/data.d
@@ -0,0 +1,11 @@
+data.d: ../data.cpp ../data.h \
+ /home/fedora/workspace/libgram/libgram/query.h ../encoding.h \
+ ../utf8codec.h
+
+../data.h:
+
+/home/fedora/workspace/libgram/libgram/query.h:
+
+../encoding.h:
+
+../utf8codec.h:
