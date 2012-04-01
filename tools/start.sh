d8a9fd33c6911db328e4f1e7ed783d03a42d2b61
Tomasz Mikolajewski <asanoki@gmail.com>
2012-04-01 16:47:08 +0200

    Starting script runs gramd in daemon mode now.

--- tools/start.sh
+++ tools/start.sh
@@ -2,10 +2,10 @@
 
 AUTO_PORT_FILENAME=/tmp/.gramd
 DICTIONARIES="../data/ngrams-2-big.txt"
-BIN_COMMAND=gramd # <- Change it if installed in custom location
+BIN_COMMAND=gramd # <- Change it if installed in a custom location
 
-OPTIONS="--interactive --verbose --epsilon 0.0001"
-#OPTIONS="--verbose --epsilon 0.001"
+#OPTIONS="--interactive --verbose --epsilon 0.0001"
+OPTIONS="--verbose --epsilon 0.0001"
 
 `killall gramd 2> /dev/null`
 if [ $? -eq 0 ]; then
